cbe4c1d3fe83f18ff0577e88366a9ad8ff15b5db
mirazfaruk <mirazfaruk@gmail.com>
2024-06-03 17:08:05 +0600

    Added nav bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import './App.css'
+import Header from './components/Header/Header'
 
 function App() {
 
   return (
     <>
-      <h1>Flavor Fusion</h1>
+      <Header></Header>
     </>
   )
 }
